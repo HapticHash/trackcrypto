c567675557e953e19ede2fa99ade6dd76991f277
HapticHash <harshitks2203@gmail.com>
2021-02-13 18:52:13 -0500

    done mapping

--- src/Coin.js
+++ src/Coin.js
@@ -1,6 +1,14 @@
 import React from "react";
 
-const Coin = ({ name, image, symbol, price, volume }) => {
+const Coin = ({
+  name,
+  image,
+  symbol,
+  price,
+  volume,
+  priceChange,
+  marketCap,
+}) => {
   return (
     <div className="coin__container">
       <div className="coin__row">
@@ -12,6 +20,14 @@ const Coin = ({ name, image, symbol, price, volume }) => {
         <div className="coin__data">
           <p className="coin__price">CA${price}</p>
           <p className="coin__vol"> CA${volume.toLocaleString()}</p>
+          {priceChange < 0 ? (
+            <p className="coin__percent red"> {priceChange.toFixed(2)}% </p>
+          ) : (
+            <p className="coin__percent green"> {priceChange.toFixed(2)}% </p>
+          )}
+          <p className="coin__marketcap">
+            Mkt Cap: ${marketCap.toLocaleString()}
+          </p>
         </div>
       </div>
     </div>
